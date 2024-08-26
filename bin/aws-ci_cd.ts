dff83da2eada8d11be1057a2f54d53553eb9e122
Sidney Espirito Santo de Souza Junior <sidney.souza@compasso.com.br>
2024-08-26 13:46:40 -0300

    initial commit

---
+++ bin/aws-ci_cd.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AwsCiCdStack } from "../lib/aws-ci_cd-stack";
+
+const app = new cdk.App();
+new AwsCiCdStack(app, "AwsCiCdStack", {});
