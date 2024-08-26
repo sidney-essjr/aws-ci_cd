7dbbd9a209ff2d1fd2eb6e78d6b92364cf80e56e
Sidney Espirito Santo de Souza Junior <sidney.souza@compasso.com.br>
2024-08-26 15:46:58 -0300

    feat: add test stage and lambda stack

--- lib/aws-ci_cd-stack.ts
+++ lib/aws-ci_cd-stack.ts
@@ -1,21 +1,22 @@
 import * as cdk from "aws-cdk-lib";
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { PipelineStage } from "./PipelineStage";
 
 export class AwsCiCdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'AwesomePipeline', {
+    const pipeline = new CodePipeline(this, "AwesomePipeline", {
       pipelineName: "AwesomePipeline",
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('sidney-essjr/aws-ci_cd', 'main'),
-        commands: [
-          'npm ci',
-          'npx cdk synth'
-        ]
-      })
-    })
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("sidney-essjr/aws-ci_cd", "main"),
+        commands: ["npm ci", "npx cdk synth"],
+      }),
+    });
 
+    const stage = pipeline.addStage(new PipelineStage(this, 'PipelineStage', {
+      stageName: "dev"
+    }));
   }
 }
